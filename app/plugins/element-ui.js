ff74cb5e4662019306f4573ad34bcebad83737a3
kidow <wcgo2ling@hanmail.net>
2019-09-06 01:18:41 +0900

    styles set

--- app/plugins/element-ui.js
+++ app/plugins/element-ui.js
@@ -1,5 +1,30 @@
 import Vue from 'vue'
 import Element from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale/lang/ko'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(Element, { locale })
+export default () => {
+  Vue.use(Element, { locale }),
+    Vue.mixin({
+      methods: {
+        notifySuccess(message) {
+          this.$notify({ type: 'success', title: '성공', message })
+        },
+        notifyError(message) {
+          this.$notify({
+            type: 'error',
+            title: '실패',
+            message:
+              message ||
+              '죄송합니다. 서버 오류가 발생했습니다. 나중에 다시 시도해주세요.'
+          })
+        },
+        notifyWarning(message) {
+          this.$notify({ type: 'warning', title: '경고', message })
+        },
+        notifyInfo(message) {
+          this.$notify({ type: 'info', title: '정보', message })
+        }
+      }
+    })
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,59 @@
+const { NODE_ENV } = process.env
+const TITLE = 'Kidolog'
+const DESCRIPTION = "Kidow's Blog"
+const IMAGE = ''
+const PUBLIC_LINK = 'https://www.kidolog.com'
+const COLOR = '#fd7e14'
+const BASE_URL =
+  NODE_ENV === 'production' ? PUBLIC_LINK : 'http://localhost:3000'
+
 module.exports = {
   mode: 'universal',
   srcDir: 'app/',
   head: {
-    title: process.env.npm_package_name || '',
+    title: TITLE,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: DESCRIPTION
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700&display=swap&subset=korean'
+      }
+    ]
   },
+  env: { BASE_URL },
+  loading: { color: COLOR },
 
-  loading: { color: '#fff' },
-
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: [{ src: '~/assets/scss/main.scss', lang: 'scss' }],
 
   plugins: ['@/plugins/element-ui'],
 
-  buildModules: [],
-
   modules: ['@nuxtjs/axios', '@nuxtjs/pwa'],
 
-  axios: {},
+  axios: {
+    baseURL: BASE_URL,
+    credentials: true,
+    proxy: false,
+    debug: false,
+    retry: {
+      retries: NODE_ENV === 'production' ? 3 : 0
+    },
+    requestInterceptor: config => {
+      config.headers.common['Authorization'] = ''
+      config.headers.common['Content-Type'] =
+        'application/x-www-form-urlencoded;application/json'
+      return config
+    }
+  },
 
   build: {
     transpile: [/^element-ui/],
