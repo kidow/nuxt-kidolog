fab8318e7dd6c13959d9e49941f0def2128ee391
kidow <wcgo2ling@hanmail.net>
2019-09-07 15:35:03 +0900

    editor separator

--- app/pages/editor/_id.vue
+++ app/pages/editor/_id.vue
@@ -19,7 +19,15 @@
         <el-button class="submit" type="primary" plain>작성하기</el-button>
       </div>
     </div>
-    <div class="editor__content"></div>
+    <div class="editor__content flex">
+      <div class="content__flex flex display" :style="{ flex: leftPercentage }">display</div>
+      <div class="content__flex flex none" :style="{ flex: 1 - leftPercentage}">none</div>
+      <div
+        class="content__separator"
+        @mousedown="onSeparatorMouseDown"
+        :style="{ left: `${leftPercentage * 100}%` }"
+      />
+    </div>
   </div>
 </template>
 
@@ -30,7 +38,19 @@ export default {
     title: '',
     leftPercentage: 0.5
   }),
-  methods: {}
+  methods: {
+    onSeparatorMouseDown() {
+      document.body.addEventListener('mousemove', this.onMouseMove)
+      window.addEventListener('mouseup', this.onMouseUp)
+    },
+    onMouseMove(e) {
+      this.leftPercentage = e.clientX / window.innerWidth
+    },
+    onMouseUp() {
+      document.body.removeEventListener('mousemove', this.onMouseMove)
+      window.removeEventListener('mouseup', this.onMouseUp)
+    }
+  }
 }
 </script>
 
@@ -77,6 +97,31 @@ export default {
           color: $brand-color;
         }
       }
+    }
+  }
+  .editor__content {
+    height: calc(100vh - 4rem);
+    position: relative;
+    .content__flex {
+      min-width: 0;
+      overflow: auto;
+      &.display {
+        @include media('<phone') {
+          flex: 1 !important;
+        }
+      }
+      &.none {
+        @include media('<phone') {
+          display: none !important;
+        }
+      }
+    }
+    .content__separator {
+      width: 1rem;
+      height: 100%;
+      position: absolute;
+      transform: translate(-50%);
+      cursor: col-resize;
     }
   }
 }
